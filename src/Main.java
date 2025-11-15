e866cc401e7e668efbf5fb606afe9873bb5d2f60
jmchairez2004-stack <jmchairez2004@gmail.com>
2025-11-15 16:55:56 -0700

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -3,15 +3,14 @@ import java.util.Scanner;
 /**
  * The Main class serves as the entry point for the Gym Management System.
  * It creates a GymSystem object and provides an interactive console-based
- * menu that allows the user to perform administrative actions such as
- * adding members, adding trainers, displaying members, and exiting the system.
+ * menu that allows the user register as
+ * a member, trainer or an admin, and exiting the system.
  */
 
 public class Main {
     /**
      * The main method begins execution of the program.
-     * It initializes the GymSystem and Scanner, displays menu options
-     * in a loop, and processes user input using a switch statement.
+     * It initializes the GymSystem and Scanner, displays menu options.
      *
      * @param args command-line arguments (not used in this program)
      */
@@ -24,15 +23,12 @@ public class Main {
             int options = input.nextInt();
             switch (options) {
                 case 1:
-                    // add member
+                    // register
                     break;
                 case 2:
-                    // add trainer
+                    // login
                     break;
                 case 3:
-                    // display members
-                    break;
-                case 4:
                     running = false;
                     System.out.println("Exiting system...");
                     break;
