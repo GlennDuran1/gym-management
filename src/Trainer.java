b89438bacf80949187318b166906702109d1d5c7
jmchairez2004-stack <jmchairez2004@gmail.com>
2025-11-15 16:21:24 -0700

    Update Trainer.java
    java doc comments

--- src/Trainer.java
+++ src/Trainer.java
@@ -1,23 +1,45 @@
+/**
+ * The Trainer class represents a fitness trainer in the GymSystem.
+ * Each trainer has a name and a specialty area they focus on.
+ */
+
 public class Trainer {
 
     //Attributes
     private String name;
     private String specialty;
 
-    //Constructor
+     /**
+     * Constructs a new Trainer with the specified name and specialty.
+     *
+     * @param name       the trainer's full name
+     * @param specialty  the trainer's area of specialization
+     */
     public Trainer(String name, String specialty) {
     
     }
 
     //Getters & Setters
+    /**
+     * Returns the trainer's full name.
+     * @return the name of the trainer
+     */
     public String getName() {
         return name;
      }
+
+    /**
+     * Returns the area of expertise of the trainer.
+     * @return the trainer's specialty
+     */
     public String getSpecialty() {
         return specialty;
      }
 
-    //Method that prints info on specific trainer
+    /**
+     * Displays the trainer's information, including
+     * name and area of specialty.
+     */
     public void displayInfo() {
     
     }
