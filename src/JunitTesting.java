efe9737626994d2c8e761e41d392822136a7a10d
vixby49 <mbrlonk@gmail.com>
2025-11-16 22:38:28 -0700

    Update JunitTesting.java

--- src/JunitTesting.java
+++ src/JunitTesting.java
@@ -2,9 +2,11 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 @Test
-void testAddMember{
-  Member membertest
-  
+void createMember{
+  Member membertest = new Member("Mike", "MikeBeast998", "Mike'sGymPassword", "1", "Basic");
+  assertEquals("Mike", membertest.getName());
+  assertEquals("MikeBeast998", membertest.getUserName());
+  assertEquals("Mike'sGymPassword", membertest.getName());
   
 }
 
