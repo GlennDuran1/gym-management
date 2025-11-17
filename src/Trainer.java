e23e82870799c18f5ce73abd7b6415a7ff936a88
jmchairez2004-stack <jmchairez2004@gmail.com>
2025-11-16 22:00:56 -0700

    Update Trainer.java

--- src/Trainer.java
+++ src/Trainer.java
@@ -3,44 +3,42 @@
  * Each trainer has a name and a specialty area they focus on.
  */
 
-public class Trainer extends Person{
+public class Trainer extends Person {
     private String specialty;
-     /**
+
+    /**
      * Constructs a new Trainer with the inherited name and specialty.
      *
-     * @param name       the trainer's full name
-     * @param userName   the trainers username
-     * @param password   the trainer's password
-     * @param id         the trainer's id
-     * @param specialty  the trainer's area of specialization
+     * @param name      the trainer's full name
+     * @param userName  the trainers username
+     * @param password  the trainer's password
+     * @param id        the trainer's id
+     * @param specialty the trainer's area of specialization
      */
     public Trainer(String name, String userName, String password, int id, String specialty) {
-    super(name, userName, password, id);
-    this.specialty = specialty;
+        super(name, userName, password, id);
+        this.specialty = specialty;
     }
 
-    //Getter
+    // Getter
     /**
      * Returns the area of expertise of the trainer.
+     * 
      * @return the trainer's specialty
      */
     public String getSpecialty() {
         return specialty;
-     }
-     //setter
-     public void setSpecialty(String specialty) {
-    this.specialty = specialty;
-}
+    }
+
+    // setter
+    public void setSpecialty(String specialty) {
+        this.specialty = specialty;
+    }
 
     /**
      * Displays the trainer's information, including
      * name and area of specialty.
      */
-    public void displayInfo() {
-        System.out.println("Name: " + name);
-        System.out.println("Username: " + userName);
-        System.out.println("Specialty: " + specialty);
-    }
     @Override
     public String toString() {
         return "Trainer: " + name + " | Username: " + userName + " | Specialty: " + specialty;
