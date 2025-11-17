233364a386f1accf9e05648a9d129394fa4bf576
jmchairez2004-stack <jmchairez2004@gmail.com>
2025-11-16 21:59:51 -0700

    Update Member.java

--- src/Member.java
+++ src/Member.java
@@ -4,51 +4,47 @@
  * selected membership type.
  */
 
-public class Member extends Person{
+public class Member extends Person {
 
-    //Attributes
+    // Attributes
     private String membershipType;
 
-    
     /**
      * Constructs a new Member object with the given information.
      *
-     * @param name            the member's full name
-     * @param username        the member's chosen username
-     * @param password        the password for the account
-     * @param id              the id unique to member
-     * @param membershipType  the membership plan the member has selected
+     * @param name           the member's full name
+     * @param username       the member's chosen username
+     * @param password       the password for the account
+     * @param id             the id unique to member
+     * @param membershipType the membership plan the member has selected
      */
     public Member(String name, String username, String password, int id, String membershipType) {
         super(name, username, password, id);
         this.membershipType = membershipType;
     }
 
-    //Getter
+    // Getter
     /**
      * Returns the member's membership type.
+     * 
      * @return the membership type
      */
     public String getMembershipType() {
         return membershipType;
-     }
-     //Setter
-     public void setMembershipType(String membershipType) {
-    this.membershipType = membershipType;
-}
+    }
 
-     //method
+    // Setter
+    public void setMembershipType(String membershipType) {
+        this.membershipType = membershipType;
+    }
+
+    // method
     /**
      * Displays the member's information, including name,
      * username, and membership type.
      */
-    public void displayInfo() {
-        System.out.println("Name: " + name);
-        System.out.println("Username: " + userName);
-        System.out.println("Membership: " + membershipType);
-    }
     @Override
-    public String toString(){   
+    public String toString() {
         return "Member: " + name + " | Username: " + userName + " | Plan: " + membershipType;
     }
 }
