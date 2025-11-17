659ab478f0cb3a0f53a1217241591d7398484474
vixby49 <mbrlonk@gmail.com>
2025-11-16 23:10:56 -0700

    Update JunitTesting.java

--- src/JunitTesting.java
+++ src/JunitTesting.java
@@ -3,7 +3,7 @@ import org.junit.jupiter.api.Test;
 
 
 @Test
-void testMember{
+void testMember(){
   Member membertest = new Member("Mike", "MikeBeast998", "Mike'sGymPassword", "1", "Basic");
   assertEquals("Mike", membertest.getName());
   assertEquals("MikeBeast998", membertest.getUserName());
@@ -13,19 +13,36 @@ void testMember{
 }
 
 @Test
-void testTrainer{
-  Trainer trainertest = new Trainer("Alice Dow", "alice", "password123", 2, "Yoga");
-  assertEquals("Yoga", trainertest.getSpeciality());
+void testTrainer(){
+  Trainer trainertest = new Trainer("Haru Ur", "haru", "password123", 2, "Sprint");
+  assertEquals("Sprint", trainertest.getSpeciality());
   trainertest.setSpecialty("Box");
   assertEquals("Box", trainertest.getSpeciality());
-  assertTrue(trainertest.checkCredentials("alice", "pass123"));
+  assertTrue(trainertest.checkCredentials("haru", "pass123"));
 }
 
 @Test
-void testAdmin{
+void testAdmin(){
   GymSystem systemtest = new GymSystem();
   Admin admintest = new Admin("Admin User", "admin", "adminpass", 3, systemtest);
   assertEquals("Admin User", admin.getName());
   assertEquals("admin", admin.getUserName());
   assertTrue(admin.checkCredentials("admin", "adminpass"));
+}
+
+@Test
+void testUsernameUnique(){
+  Member membertest = new Member("Mike", "MikeBeast998", "Mike'sGymPassword", "1", "Basic");
+  Trainer trainertest = new Trainer("Haru Ur", "haru", "password123", 2, "Sprint");
+  GymSystem systemtest = new GymSystem();
+  Admin admintest = new Admin("Admin User", "admin", "adminpass", 3, systemtest);
+  systemtest.addMember(member);
+  systemtest.addTrainer(trainer);
+  systemtest.addAdmin(admin);
+
+  assertTrue(system.isUsernameTaken("haru"));
+  assertFalse(system.isUsernameTaken("lily"));
+  assertTrue(system.isUsernameTaken("admin"));
+  assertFalse(system.isUsernameTaken("mark"));
+  assertTrue(system.isUsernameTaken("MikeBeast998"));
 }
