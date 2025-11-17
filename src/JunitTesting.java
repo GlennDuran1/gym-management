2920180cff3569501c4e78b561f36dbc9b448041
vixby49 <mbrlonk@gmail.com>
2025-11-16 22:59:10 -0700

    Update JunitTesting.java

--- src/JunitTesting.java
+++ src/JunitTesting.java
@@ -1,21 +1,27 @@
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
+
 @Test
-void createMember{
+void testMember{
   Member membertest = new Member("Mike", "MikeBeast998", "Mike'sGymPassword", "1", "Basic");
   assertEquals("Mike", membertest.getName());
   assertEquals("MikeBeast998", membertest.getUserName());
   assertEquals(10, membertest.getId());
   assertEquals("Basic", membertest.getMembershipType());
+  assertTrue(membertest.checkCredentials("johnd", "pass123"));
 }
 
 @Test
-void createMember{
-  Member membertest = new Member("Mike", "MikeBeast998", "Mike'sGymPassword", "1", "Basic");
-  assertEquals("Mike", membertest.getName());
-  assertEquals("MikeBeast998", membertest.getUserName());
-  assertEquals(10, membertest.getId());
-  assertEquals("Basic", membertest.getMembershipType());
+void testTrainer{
+  Trainer trainertest = new Trainer("Alice Dow", "alice", "password123", 2, "Yoga");
+  assertEquals("Yoga", trainertest.getSpeciality());
+  trainertest.setSpecialty("Box");
+  assertEquals("Box", trainertest.getSpeciality());
+  assertTrue(trainertest.checkCredentials("alice", "pass123"));
 }
 
+@Test
+void testAdmin{
+   
+}
